40d6d6240e0631c643184308881a48fdf418de87
vagrant <vagrant@vagrant.vm>
2020-09-30 20:10:28 +0000

    Begins while statement, asks for and tokenizes user input

--- calculator.py
+++ calculator.py
@@ -4,9 +4,14 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 # While statement
-
+while True:
     # Capture user input
+    user_input = input("Enter your equation> ")
+
     # Tokenize input
+    tokenized_input = user_input.split(" ")
+    
+    # Check if first token is "q"
 
     # Assign individual tokens to variables
 
